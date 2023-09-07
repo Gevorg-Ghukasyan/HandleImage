e8c21bb7c47cb4128de08e229c908866d5d15897
Asus <Asus@DESKTOP-FVC21D3>
2023-09-07 16:04:17 +0400

    Добавьте файлы проекта.

---
+++ HandleImage/Pixel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandleImage
+{
+    public class Pixel
+    {
+        public Point Point {get; set;}
+        public Color Color { get; set; }    
+    }
+}
